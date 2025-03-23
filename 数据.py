e23a84dbb08700c546e7e41106737c8241912bbc
L-LunAr <131319007+L-LunAr@users.noreply.github.com>
2025-03-23 08:55:10 +0800

    Update 数据.py

--- 数据.py
+++ 数据.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 #from pymysql import connect
 #import pymysql
-DATA_URL="shuju_total.csv"
+DATA_URL="shuju500.xlsx"
 #from pages import mysql_connect
 st.title('均质油藏压力和压力导数数据')
 @st.cache_data
