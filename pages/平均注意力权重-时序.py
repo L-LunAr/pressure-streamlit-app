7d1eb2d5e71f9adf646dc793c9896b57aaffb483
L-LunAr <131319007+L-LunAr@users.noreply.github.com>
2025-03-23 08:55:56 +0800

    Update 平均注意力权重-时序.py

--- pages/平均注意力权重-时序.py
+++ pages/平均注意力权重-时序.py
@@ -10,7 +10,7 @@ st.sidebar.header("平均注意力权重得分")
 st.write(
     """测试集数据注意力权重得分值取平均，展示100个时序数据点注意力权重得分值,每25个得分数据展示一次折线图！"""
 )
-excel_file_path = r'D:\LunAr-chengxu\jupyter-L\1DNN-LSTM_att_3ymodel_Top25Values.xlsx'
+excel_file_path = '1DNN-LSTM_att_3ymodel_Top25Values.xlsx'
 # 初始化进度条和状态文本
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
